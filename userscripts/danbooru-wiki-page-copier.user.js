6cc6af5c2e931d5e3adee78f4ae004d544c912cd
noirscape <neko@catgirlsin.space>
2024-08-29 20:13:37 +0200

    Update danbooru-wiki-page-copier.user.js

--- userscripts/danbooru-wiki-page-copier.user.js
+++ userscripts/danbooru-wiki-page-copier.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Danbooru wiki page copier
-// @version      4
+// @version      5
 // @match        *://danbooru.donmai.us/wiki_pages/*
 // @grant        GM.xmlHttpRequest
 // @require      https://openuserjs.org/src/libs/sizzle/GM_config.js
@@ -61,7 +61,7 @@ function CreateBooruPage (aEvent) {
                        "responseType": "json",
                        "onload": function(response) {
                           let jdata = response.response;
-		          let destArgs = encodeURIComponent(`wiki_page[title]=${jdata.title}&wiki_page[body]=${jdata.body}&wiki_page[other_names_string]=${jdata.other_names.join(' ')})`
+		          let destArgs = encodeURIComponent(`wiki_page[title]=${jdata.title}&wiki_page[body]=${jdata.body}&wiki_page[other_names_string]=${jdata.other_names.join(' ')}`);
                           let destURL = `${gmcfg.get('booruDomain')}/wiki_pages/new?{destArgs}`;
                           window.location = destURL;
     			}}
